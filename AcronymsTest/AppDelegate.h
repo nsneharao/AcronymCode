18b2d6aacb6cb5ed7aecfc60e1746962f5c2f49c
Mohnish Vardhan Goud <vmohnish@thisisswitch.com>
2017-04-21 06:22:09 +0530

    Initial Commit

---
+++ AcronymsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcronymsTest
+//
+//  Created by Mohnish vardhan on 21/04/17.
+//  Copyright © 2017 Mohnish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcronymsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcronymsTest
+//
+//  Created by Mohnish vardhan on 21/04/17.
+//  Copyright © 2017 Mohnish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
